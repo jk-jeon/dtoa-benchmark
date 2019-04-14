01e86ebbc53f0d69f48a8718d77d4e14144d7043
Junekey Jeon <jk_jeon@kaist.ac.kr>
2019-04-15 05:59:01 +0900

    Fixed to_chars.cpp and updated result

--- src/to_chars.cpp
+++ src/to_chars.cpp
@@ -2,7 +2,7 @@
 #include "test.h"
 
 void dtoa_to_chars(double value, char* buffer) {
-	auto result = std::to_chars(buffer, buffer + 1023, value);
+	auto result = std::to_chars(buffer, buffer + 45, value);
 	*result.ptr = '\0';
 }
 
